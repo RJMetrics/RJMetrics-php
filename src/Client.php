b488f7a24f1ab9c2e0220a89afd9cd55b0cf64e6
Connor McArthur <cmcarthur@rjmetrics.com>
2014-01-21 14:55:53 -0500

    functional test tweaks

--- src/Client.php
+++ src/Client.php
@@ -4,22 +4,22 @@ namespace RJMetrics;
 
 use Httpful;
 
-class UnableToConnectException extends Exception {}
+class UnableToConnectException extends \Exception {}
 
 class Client {
 
-	const API_BASE = "https://connect.rjmetrics.com/v2"
-	const SANDBOX_BASE = "https://sandbox-connect.rjmetrics.com/v2"
+	const API_BASE = "https://connect.rjmetrics.com/v2";
+	const SANDBOX_BASE = "https://sandbox-connect.rjmetrics.com/v2";
 
 	private $clientId, $apiKey;
 
 	public function __construct($clientId, $apiKey) {
 		if(!is_int($clientId) || $clientId <= 0)
-			throw new InvalidArgumentException(
+			throw new \InvalidArgumentException(
 				"Invalid client ID: {$clientId} -- must be a positive integer.");
 
 		if(!is_string($apiKey))
-			throw new InvalidArgumentException(
+			throw new \InvalidArgumentException(
 				"Invalid API key: {$apiKey} -- must be a string.");
 
 		$this->clientId = $clientId;
@@ -32,28 +32,30 @@ class Client {
 	public function test() {
 		$testData = json_decode("[{\"keys\":[\"id\"],\"id\":1}]");
 
-		return $this->pushData($testData, self::SANDBOX_BASE)["code"] == 201;
+		return $this->pushData("test", $testData, self::SANDBOX_BASE)["code"] == 201;
 	}
 
 	public function pushData($table, $data, $url = self::API_BASE) {
 		if(!is_object($data) && !is_array($data))
-			throw new InvalidArgumentException(
+			throw new \InvalidArgumentException(
 				"Invalid data -- must be a valid PHP array or object.");
 
 		if(!is_string($table))
-			throw new InvalidArgumentException(
+			throw new \InvalidArgumentException(
 				"Invalid table name: '{$table}' -- must be a string.");
 
 		if(!is_array($data))
 			$data = [$data];
 
-		array_map(function($subArray) {
-			print_r($this->makePushDataAPICall($table, $subArray, $url));
-		}, array_chunk($data, 100);
+		array_map(function($subArray) use ($table, $url) {
+			$this->makePushDataAPICall($table, $subArray, $url);
+		}, array_chunk($data, 100));
 	}
 
 	private function makePushDataAPICall($table, array $data, $url = self::API_BASE) {
-		$response = \Httpful\Request::post("{$url}/client/{$this->clientId}/table/test/data?{$this->apiKey}")
+		$requestUrl = "{$url}/client/{$this->clientId}/table/test/data?{$this->apiKey}";
+
+		$response = \Httpful\Request::post($requestUrl)
 			->mime("application/json")
 			->body($data)
 			->send();
